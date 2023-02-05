55d26fd3a491e6bce556075c64b705b8151fb20d
diemonch <ravishankard_2k9@yahoo.com>
2023-02-05 16:40:17 +0530

    Added dependencies

--- Cloudweather.Percipitation/Program.cs
+++ Cloudweather.Percipitation/Program.cs
@@ -1,25 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-//builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+app.Map("/observation/{zip}", (string zip, [FromQuery] int ? days) =>
+{
+    return Results.Ok(zip);
 
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
+});
 
 app.Run();
